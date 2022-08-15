991e66c583382475c697ff4c4f48d2fdb72941f4
Nitchanan-K <teelajen111@gmail.com>
2022-08-15 17:33:39 +0700

    add scaner knltnal

--- ttm-squeeze/squeeze.py
+++ ttm-squeeze/squeeze.py
@@ -15,6 +15,7 @@ for filename in os.listdir('datasets/daily_07-08'):
 
     # cal 20 sma
     df['20sma'] = df['close'].rolling(window=20).mean()
+
     # make bb band
     # std dev
     df['stddev'] = df['close'].rolling(window=20).std()
@@ -22,17 +23,39 @@ for filename in os.listdir('datasets/daily_07-08'):
     df['lower_band'] = df['20sma'] - (2 * df['stddev'])
     df['upper_band'] = df['20sma'] + (2 * df['stddev'])
 
-    if symbol in symbols:
+    # Ture range
+    df['TR'] = abs(df['high'] - df['low'])
+    df['ATR'] = df['TR'].rolling(window=20).mean()
+
+    df['lower_keltner'] = df['20sma'] - (df['ATR'] * 1.5)
+    df['upper_keltner'] = df['20sma'] + (df['ATR'] * 1.5)
+
+    def in_squeeze(df):
+        return df['lower_band'] > df['lower_keltner'] and df['upper_band'] < df['upper_keltner']
+
+    df['squeeze_on'] = df.apply(in_squeeze, axis=1)
+
+    if df.iloc[-1]['squeeze_on']:
+        print(f"{symbol} is in the squeeze!!!")
+
+    if symbol == "KSM":
         print(df)
         eth_df = df
 
+
+
+# Define plot
 candlestick = go.Candlestick(x=eth_df['date'],open=eth_df['open'],high=eth_df['high'],low=eth_df['low'],close=eth_df['close'])
 upper_band = go.Scatter(x=eth_df['date'],y=eth_df['upper_band'], name='Upper Bollinger Band', line={'color':'red'})
 lower_band = go.Scatter(x=eth_df['date'],y=eth_df['lower_band'], name='Lower Bollinger Band', line={'color':'red'})
+upper_keltner = go.Scatter(x=eth_df['date'],y=eth_df['upper_keltner'], name='Upper Keltner Channel', line={'color':'blue'})
+lower_keltner = go.Scatter(x=eth_df['date'],y=eth_df['lower_keltner'], name='Lower Keltner Channel', line={'color':'blue'})
 
 
 
-fig = go.Figure(data=[candlestick,upper_band,lower_band])
+
+
+fig = go.Figure(data=[candlestick,upper_band,lower_band,upper_keltner,lower_keltner])
 
 fig.layout.xaxis.type = 'category'
 fig.layout.xaxis.rangeslider.visible = False
